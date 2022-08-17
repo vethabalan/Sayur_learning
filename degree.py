ea5fb2f258515cdc7f9e0c9cac7209f10763fc28
vethabalan <110384504+vethabalan@users.noreply.github.com>
2022-08-17 20:07:37 +0530

    Add files via upload

---
+++ degree.py
@@ -0,0 +1,30 @@
+#degree
+
+mark_1 = int(input("enter mark_1: "))
+mark_2 = int(input("enter mark_2: "))
+mark_3 = int(input("enter mark_3: "))
+mark_4 = int(input("enter mark_4: "))
+mark_5 = int(input("enter mark_5: "))
+
+
+max_mark=60
+min_mark = 0
+if((mark_1>=max_mark) and (mark_2>=max_mark) and (mark_3>=max_mark) and (mark_4>=max_mark) and (mark_5>=max_mark)):
+    print("pass")
+
+    
+    max_mark = 90
+    min_mark = 40
+elif((mark_1>=max_mark) and (mark_2>=max_mark) and (mark_3>=max_mark) and (mark_4>=min_mark) and (mark_5>=min_mark)):
+    print("pass")
+
+
+    max_mark = 75
+    min_mark = 50
+
+elif((mark_1 >=max_mark) and (mark_2 >= max_mark) and (mark_3 >= max_mark) and (mark_4 >= min_mark)and (mark_5 >= min_mark)):
+    print("pass")
+    
+else:
+    print("fail")
+    
